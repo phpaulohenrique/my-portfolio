a4a37557617e75c5e0b2e39488e0569a4cf0c603
Paulo Henrique <paulo.henrique1080p@gmail.com>
2022-06-16 20:56:45 -0300

    1:CSS Improvements. 2:JS Improvements. 3:Resposive Improvements. 4:Implemented contact guide.

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,21 @@
 ScrollReveal({
-  origin: "left",
-  distance: "100px",
-  duration: 1500,
+    origin: "left",
+    distance: "100px",
+    duration: 1500,
 }).reveal(`
     #header h1`);
 
-
 ScrollReveal({
-  origin: "right",
-  distance: "100px",
-  duration: 1500,
+    origin: "right",
+    distance: "100px",
+    duration: 1500,
 }).reveal(`
     #header img`);
 
-
-
-
 ScrollReveal({
-  origin: "bottom",
-  distance: "70px",
-  duration: 2500,
+    origin: "bottom",
+    distance: "70px",
+    duration: 2500,
 }).reveal(`
     #main__section-hello, #main__section-hello,
     #main__section-about-me h1,
@@ -29,3 +25,82 @@ ScrollReveal({
     .main__section-projects__link-github,
     #main__section-knowledge h1,
     .main__section-knowledge__icons img`);
+
+
+
+
+const imgsTechnologies = document.querySelectorAll(".main__section-knowledge__icons img");
+
+const textsTechnologies = document.querySelectorAll(
+    ".main__section-knowledge__techs p"
+);
+
+const textMouseOverText = document.querySelector('.main__section-knowledge__techs__instruction')
+
+let isMobile = window.matchMedia("(pointer:coarse)").matches;
+
+if(isMobile){
+    textMouseOverText.innerText = '*Clique em uma das tecnologias para ler a descrição!'
+}
+
+console.log(isMobile);
+
+// fromArray()
+console.log(textsTechnologies);
+
+const technologiesTexts = Array.from(textsTechnologies);
+
+console.log(technologiesTexts)
+
+// const technologiesThatIKnow = technologiesTexts.reduce((pastTechnology, currentTechnology) => {
+//     return [
+//         ...pastTechnology,
+//         currentTechnology.getAttribute('name'),
+//     ];
+// }, []);
+
+// console.log(technologiesThatIKnow)
+
+function hideTechnologyText(){
+    textsTechnologies.forEach((text) => {
+        text.classList.remove("is--active");
+    });
+
+}
+
+imgsTechnologies.forEach((img) => { img.addEventListener("mouseover", () => {
+    // console.log(img.name)
+    // let i
+    // const a = textsTechnologies.forEach((text) =>
+    //     i = text.getAttribute("name") == 'js'
+        
+    //     // console.log(text.getAttribute("name")
+    // );
+    // technologiesThatIKnow
+
+    hideTechnologyText();
+
+
+    function findIndexTechnologyTextOnMouseOverImg(textTechnology){
+        return textTechnology.getAttribute("name") == img.name;
+
+    }
+    console.log(technologiesTexts.findIndex(findIndexTechnologyTextOnMouseOverImg));
+
+    const indexTechnologyTextMouseOver = technologiesTexts.findIndex(findIndexTechnologyTextOnMouseOverImg);
+
+    textsTechnologies[indexTechnologyTextMouseOver].classList.add("is--active");
+    
+    // setTimeout(hideTechnologyText, 12000);
+
+    // console.log(i)
+    // console.log(nameAtributeTextTech);
+    // console.log(img.name == nameAtributeTextTech);
+    // console.log(a)
+});})
+
+
+// img.addEventListener('click', () => {
+//   alert('eita')
+// })
+
